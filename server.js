820210c61ddc447ff43ba59662abca900efd2efa
Alec Chan <me@alecchan.io>
2018-05-24 18:12:32 -0700

    configured server for heroku concurrency

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-var express  = require('express');
-var app      = express();                               
-var morgan = require('morgan');            
-var bodyParser = require('body-parser');    
-var cors = require('cors');
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-app.use(morgan('dev'));                                        
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
-app.use(cors());
- 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
- 
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
 
+function start() {
+  const express  = require('express');
+  const app      = express();                               
+  const morgan = require('morgan');            
+  const bodyParser = require('body-parser');    
+  const cors = require('cors');
+
+  app.use(morgan('dev'));                                        
+  app.use(bodyParser.urlencoded({'extended':'true'}));            
+  app.use(bodyParser.json());                                     
+  app.use(cors());
+   
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+   
+  app.use(express.static('www'));
+  app.set('port', process.env.PORT || 3000);
+  app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+}
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import * as firebase from 'firebase';
 import { WelcomePage } from '../pages/welcome/welcome';
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
   rootPage:any = WelcomePage;
-
+  config = {
+    apiKey: "AIzaSyBQnYkNcTjiWhTtB8E5MSO2LJ7z9qN9ckg",
+    authDomain: "sync-9192c.firebaseapp.com",
+    databaseURL: "https://sync-9192c.firebaseio.com",
+    projectId: "sync-9192c",
+    storageBucket: "sync-9192c.appspot.com",
+    messagingSenderId: "69668555707"
+  };
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
     });
+    firebase.initializeApp(this.config);
   }
 }
 
