310a560d5b18204195f4cda302b5c1b10b632f28
Alec Chan <me@alecchan.io>
2018-05-24 23:56:30 -0700

    finished login and register pages

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-
+import * as $ from 'jquery';
+import firebase from 'firebase';
+import { HomePage } from '../home/home';
 /**
- * Generated class for the LoginPage page.
+ * Generated class for the loginPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -16,10 +18,8 @@ import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginPage {
   loginForm: FormGroup;
-  submitAttempt: boolean = false;
-  current = null;
-  
-
+  loggedin;
+  error = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     const fb = new FormBuilder();
     this.loginForm = fb.group({
@@ -27,68 +27,84 @@ export class LoginPage {
       password: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
     });
   }
+  ionViewDidLoad() {
+    this.loggedin = $(".loggedin").hide();
+  }
+  save() {
+    const t = 500;
+    const that = this;
+    firebase.auth().signInWithEmailAndPassword(that.loginForm.controls['email'].value, that.loginForm.controls['password'].value).then(user => {
+        $(".login_inner__check").css({
+          'animation': '',
+        });
+        $(".login_inner__check--complete").find('ion-icon').animate({
+          'opacity': '1'
+        }, 500);
+        setTimeout(()=>{
+          // fade out the login board
+          $(".login").fadeOut(500, function() {
+            $(this).hide();
+          });
+          setTimeout(()=>{
+            // on sucess reveal background and then 500ms later reveal welcome text
+            $(".header").hide();
+              
+            $(".fixed-content, .scroll-content").css("margin-top", 0);
+            
+            this.loggedin.fadeIn(t, function() {
+              $(this).show();
+              $(this).find('h2').html("Welcome back, " + firebase.auth().currentUser.displayName);
+              setTimeout(()=>{
+                $(".loggedin h2").animate({
+                  'opacity': '1'
+                }, t);
+                setTimeout(() => {
+                  that.navCtrl.setRoot(HomePage);
+                }, 2300);
+              }, 500);
+            });
+          },500);
+        },1600);
+      
+    }).catch(err => {
+      this.error = true;
+      setTimeout(()=>{
+        $(".login_inner__check").css({
+          'animation': '',
+        });
+        $(".login_inner__check--complete").find('ion-icon').animate({
+          'opacity': '1'
+        }, 500);
+        setTimeout(()=> {
+          alert(err.message);
+          this.reset();
+        }, 1100);
+      },500);
+      
+    });
 
-  ionViewDidLoad() { 
-    const anime = window['anime'];
-    document.querySelector('#email').addEventListener('focus', function(e) {
-      if (this.current) this.current.pause();
-      this.current = anime({
-        targets: 'path',
-        strokeDashoffset: {
-          value: 0,
-          duration: 700,
-          easing: 'easeOutQuart'
-        },
-        strokeDasharray: {
-          value: '240 1386',
-          duration: 700,
-          easing: 'easeOutQuart'
-        }
-      });
-    });
-    document.querySelector('#password').addEventListener('focus', function(e) {
-      if (this.current) this.current.pause();
-      this.current = anime({
-        targets: 'path',
-        strokeDashoffset: {
-          value: -336,
-          duration: 700,
-          easing: 'easeOutQuart'
-        },
-        strokeDasharray: {
-          value: '240 1386',
-          duration: 700,
-          easing: 'easeOutQuart'
-        }
-      });
-    });
-    document.querySelector('#submit').addEventListener('focus', function(e) {
-      if (this.current) this.current.pause();
-      this.current = anime({
-        targets: 'path',
-        strokeDashoffset: {
-          value: -730,
-          duration: 700,
-          easing: 'easeOutQuart'
-        },
-        strokeDasharray: {
-          value: '530 1386',
-          duration: 700,
-          easing: 'easeOutQuart'
-        }
+    // after clicking login hide avatar and login
+    $(".login_inner, .login_inner__avatar").animate({
+      'opacity': '0'
+    }, t);
+
+    setTimeout(function() {
+      $(".login_inner__check").css({
+        'opacity': '1',
+        'animation': 'spinner 4s 0s linear',
+        'transition': 'all ease 3s'
       });
     });
   }
 
-  save(){
- 
-    this.submitAttempt = true;
- 
-      if(!this.loginForm.valid) {
-        console.log("success!")
-        console.log(this.loginForm.value);
-      }
- 
+  reset() {
+    this.error = false;
+    $(".login_inner, .login_inner__avatar").css('opacity', 1);
+    $(".login_inner__check").css({
+      'opacity': '0',
+      'transition': ''
+    });
+    $(".login_inner__check--complete").find('ion-icon').css('opacity', 0);
   }
 
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import * as $ from 'jquery';
+import firebase from 'firebase';
+import { HomePage } from '../home/home';
 /**
  * Generated class for the RegisterPage page.
  *
@@ -14,12 +17,96 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-
+  registerForm: FormGroup;
+  loggedin;
+  error = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    const fb = new FormBuilder();
+    this.registerForm = fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      username: ['', Validators.compose([Validators.required, Validators.maxLength(30), Validators.minLength(2)])],
+      password: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
+  }
+  ionViewDidLoad() {
+    this.loggedin = $(".loggedin").hide();
+  }
+  save() {
+    const t = 500;
+    const that = this;
+    firebase.auth().createUserWithEmailAndPassword(that.registerForm.controls['email'].value, that.registerForm.controls['password'].value).then(user => {
+      firebase.auth().currentUser.updateProfile({displayName: this.registerForm.controls['username'].value, photoURL: ''}).then(() => {
+        $(".login_inner__check").css({
+          'animation': '',
+        });
+        $(".login_inner__check--complete").find('ion-icon').animate({
+          'opacity': '1'
+        }, 500);
+        setTimeout(()=>{
+          // fade out the login board
+          $(".login").fadeOut(500, function() {
+            $(this).hide();
+          });
+          setTimeout(()=>{
+            // on sucess reveal background and then 500ms later reveal welcome text
+            $(".header").hide();
+              
+            $(".fixed-content, .scroll-content").css("margin-top", 0);
+            
+            this.loggedin.fadeIn(t, function() {
+              $(this).show();
+              $(this).find('h2').html("Welcome to Sync, " + firebase.auth().currentUser.displayName);
+              setTimeout(()=>{
+                $(".loggedin h2").animate({
+                  'opacity': '1'
+                }, t);
+                setTimeout(() => {
+                  that.navCtrl.setRoot(HomePage);
+                }, 2300);
+              }, 500);
+            });
+          },500);
+        },1600);
+      }); 
+    }).catch(err => {
+      this.error = true;
+      setTimeout(()=>{
+        $(".login_inner__check").css({
+          'animation': '',
+        });
+        $(".login_inner__check--complete").find('ion-icon').animate({
+          'opacity': '1'
+        }, 500);
+        setTimeout(()=> {
+          alert(err.message);
+          this.reset();
+        }, 1100);
+      },500);
+      
+    });
+
+    // after clicking register hide avatar and login
+    $(".login_inner, .login_inner__avatar").animate({
+      'opacity': '0'
+    }, t);
+
+    setTimeout(function() {
+      $(".login_inner__check").css({
+        'opacity': '1',
+        'animation': 'spinner 4s 0s linear',
+        'transition': 'all ease 3s'
+      });
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  reset() {
+    this.error = false;
+    $(".login_inner, .login_inner__avatar").css('opacity', 1);
+    $(".login_inner__check").css({
+      'opacity': '0',
+      'transition': ''
+    });
+    $(".login_inner__check--complete").find('ion-icon').css('opacity', 0);
   }
 
 }
