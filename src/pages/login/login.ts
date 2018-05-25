60b155bd23ae5c4b7fe88532f51fee6e05efcd1f
Alec Chan <me@alecchan.io>
2018-05-24 18:44:59 -0700

    implemented login form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,12 +15,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  loginForm: FormGroup;
+  submitAttempt: boolean = false;
+  current = null;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    const fb = new FormBuilder();
+    this.loginForm = fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  ionViewDidLoad() { 
+    document.querySelector('#email').addEventListener('focus', function(e) {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: 0,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '240 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
+    });
+    document.querySelector('#password').addEventListener('focus', function(e) {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: -336,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '240 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
+    });
+    document.querySelector('#submit').addEventListener('focus', function(e) {
+      if (this.current) this.current.pause();
+      this.current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+          value: -730,
+          duration: 700,
+          easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+          value: '530 1386',
+          duration: 700,
+          easing: 'easeOutQuart'
+        }
+      });
+    });
+  }
+
+  save(){
+ 
+    this.submitAttempt = true;
+ 
+      if(!this.loginForm.valid) {
+        console.log("success!")
+        console.log(this.loginForm.value);
+      }
+ 
   }
 
 }
