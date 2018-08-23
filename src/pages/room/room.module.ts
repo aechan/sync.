58885b6a2ecbff2f9a2a406ecb27dc8ade0a9e9f
Alec Chan <me@alecchan.io>
2018-08-23 10:00:19 -0600

    MISC: converted Room code to use ConstStrs repository instead of string literals

--- src/pages/room/room.module.ts
+++ src/pages/room/room.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RoomPage } from './room';
-import {VgCoreModule} from 'videogular2/core';
-import {VgControlsModule} from 'videogular2/controls';
-import {VgOverlayPlayModule} from 'videogular2/overlay-play';
-import {VgBufferingModule} from 'videogular2/buffering';
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgBufferingModule } from 'videogular2/buffering';
 
 @NgModule({
   declarations: [
--- src/pages/room/room.ts
+++ src/pages/room/room.ts
@@ -3,13 +3,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as $ from 'jquery';
 import { HomePage } from '../home/home';
 import firebase from 'firebase';
+import {VgAPI} from 'videogular2/core';
+import { ConstStrs } from '../../utils/Utils';
+
 @IonicPage()
 @Component({
   selector: 'page-room',
   templateUrl: 'room.html',
 })
 export class RoomPage {
-  
   roomId: string;
   hideBPB: boolean = false;
   chatLength: number = 0;
@@ -19,16 +21,53 @@ export class RoomPage {
   amOwner: boolean = false;
   dragMenuState: boolean = false; // false for up true for down
   currentVideoURL: string = "";
+  sources;
+  api:VgAPI;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.user = firebase.auth().currentUser;
     this.roomId = navParams.get('roomId');
-    firebase.database().ref('/rooms/'+this.roomId).on('value', (snap)=>{
+    firebase.database().ref(ConstStrs.ROOMS + this.roomId).on('value', (snap)=>{
       const room = snap.val();
       this.roomName = room.roomName;
+      this.amOwner = (firebase.auth().currentUser.uid === room.ownerId);
     });
-    this.amOwner = (firebase.auth().currentUser.uid === this.roomId);
   }
+
+  onPlayerReady(api:VgAPI) {
+    this.api = api;
+
+    /** setup event subscribers for video related events */
+    // subscribe to the room's video url changing
+    firebase.database().ref(ConstStrs.ROOMS + this.roomId).on('value', (snap)=> {
+      if(snap.val().currentUrl !== this.currentVideoURL) {
+        this.currentVideoURL = snap.val().currentUrl;
+        this.setCurrentVideo(this.currentVideoURL);
+      }
+    });
+  }
+
+  setCurrentVideo(source: string) {
+    if(!this.amOwner) return;
+    this.sources = new Array<Object>();
+    this.sources.push({
+      src: source,
+      type: "video/mp4"
+    });
+    this.api.getDefaultMedia().currentTime = 0;
+    this.api.play();
+  }
+
+  setVideoURL() {
+    if(!this.amOwner || $("#videoURL").val().toString().trim() === "") return;
+
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId).update({
+      currentUrl: $("#videoURL").val()
+    }).then(()=>{
+      $("#videoURL").val("");
+    });
+  }
+
 
   snapshotToArray(snapshot) {
     var returnArr = [];
@@ -46,31 +85,27 @@ export class RoomPage {
   ionViewDidLoad() {
 
     // add ourselves to the viewer list
-    firebase.database().ref('/rooms/'+this.roomId+'/viewers/'+this.user.uid).push({
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId+ConstStrs.VIEWERS+this.user.uid).push({
       name: this.user.displayName 
     });
 
-    /** setup event subscribers **/
+    /** setup event subscribers for non video actions **/
     // subscribe to number of viewers changed
-    firebase.database().ref('/rooms/'+this.roomId+'/viewers').on('value', (snap)=>{
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId+ConstStrs.VIEWERS).on('value', (snap)=>{
       let arr = this.snapshotToArray(snap);
       $("#onlineCount").text(arr.length);
     });
 
-    // subscribe to the room's video url changing
-    firebase.database().ref('/rooms/'+this.roomId)
-
-
   }
 
   ionViewWillUnload() {
-    firebase.database().ref('/rooms/'+this.roomId+'/viewers/'+this.user.uid).remove();
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId+ConstStrs.VIEWERS+this.user.uid).remove();
   }
 
   setRoomName() {
     if(!this.amOwner || $("#roomName").val().toString().trim() === "") return;
 
-    firebase.database().ref('rooms/'+this.roomId).update({
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId).update({
       roomName: $("#roomName").val(),
       roomNameLower: $("#roomName").val().toString().toLowerCase()
     }).then(()=>{
@@ -81,7 +116,7 @@ export class RoomPage {
   setRoomImage() {
     if(!this.amOwner || $("#roomImage").val().toString().trim() === "") return;
 
-    firebase.database().ref('rooms/'+this.roomId).update({
+    firebase.database().ref(ConstStrs.ROOMS+this.roomId).update({
       image: $("#roomImage").val()
     }).then(()=>{
       $("#roomImage").val("");
