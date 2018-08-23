104cd328a1fac81cda7c22455323e38e5ac05cb7
Alec Chan <me@alecchan.io>
2018-08-23 09:59:27 -0600

    CORE: created synched room model/controller and added string constants repository to reduce spelling errors

---
+++ src/models/SyncRoom.ts
@@ -0,0 +1,38 @@
+import firebase from 'firebase';
+import { ConstStrs } from '../utils/Utils';
+import { resolveDefinition } from '../../node_modules/@angular/core/src/view/util';
+import { Observable, Subscriber } from 'rxjs';
+
+/**
+ * Model/controller for room synching logic
+ */
+export class SyncRoom {
+    private roomId: string;
+
+    constructor(roomId: string, validationListener: (obs: Observable<boolean>) => any) {
+        this.roomId = roomId;
+        validationListener(this.roomIsValid);
+    }
+
+    roomIsValid = new Observable<boolean>((obs) => {
+        this.validateId().then((val) => {
+            // true if room is not invalidated
+            obs.next(val !== undefined);
+        });
+    })
+
+    get id(): Promise<string> {
+        return this.validateId()
+    }
+
+    private validateId(): Promise<string> {
+        // check if roomId snapshot does not return null from db
+        return new Promise<string>((resolve, reject) => {
+            firebase.database().ref(ConstStrs.ROOMS + this.roomId)
+                .on('value', (snap)=>{
+                    if(snap !== null) resolve(this.roomId);
+                    else resolve(undefined);
+                });
+        });
+    }
+}
---
+++ src/utils/Utils.ts
@@ -0,0 +1,5 @@
+export enum ConstStrs {
+    ROOMS = "/rooms/",
+    USERS = "/users/",
+    VIEWERS = "/viewers/"
+}
